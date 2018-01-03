b95fca57d44061aaa581fa0f9ae29160f2a8de6d
KEOTL <kento.lauzon@ligature.ca>
2018-01-03 11:53:19 -0500

    fixed typo

--- release/mcdisc-builder-1.1.1b/run.mac.sh
+++ release/mcdisc-builder-1.1.1b/run.mac.sh
@@ -2,5 +2,5 @@
 VERSION=1.1.1b
 rm mcdiscPack-*.zip
 sh prepare.mac.sh
-java -jar mcdiscResourcePackBuilder-all-${VERSION}.jar . --disc-list https://raw.githubusercontent.com/KEOTL/mcdisc/master/sample-disc-config.json --pool-size 20 --max-retry 5java -jar mcdiscResourcePackBuilder-all-${VERSION}.jar .
+java -jar mcdiscResourcePackBuilder-all-${VERSION}.jar . --disc-list https://raw.githubusercontent.com/KEOTL/mcdisc/master/sample-disc-config.json --pool-size 20 --max-retry 5
 zip -r mcdiscPack-$(date +%d-%m-%Y).zip assets pack.mcmeta
