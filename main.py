70541a16613f555f620496798272cf68ce895417
Kento A. Lauzon <kento.lauzon@ligature.ca>
2018-02-22 17:22:29 -0500

    argument parsing

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import mcdiscbuilder
+import argparse
 
 TEST_URL = "https://raw.githubusercontent.com/keotl/mcdisc/master/sample-disc-config.json"
-TARGET_DIRECTORY = "/tmp/testmcdisc"
+TARGET_DIRECTORY = "."
 
-mcdiscbuilder.build_resource_pack(TEST_URL, TARGET_DIRECTORY)
+parser = argparse.ArgumentParser(description="mcdisc resource pack builder")
+parser.add_argument(help="Disc list location. Can be an URL, or a file path.", dest="disc_list")
+args = parser.parse_args()
+
+mcdiscbuilder.build_resource_pack(args.disc_list, TARGET_DIRECTORY)
 
