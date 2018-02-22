ce29a2bd25c01028c53cf734c101355b785c4635
Atreides <kento.lauzon@ligature.ca>
2018-02-21 20:03:11 -0500

    create zip

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from mcdiscbuilder import downloader
 from mcdiscbuilder.metadata import prepare_lang_file, prepare_sounds_json, prepare_pack_info
 from mcdiscbuilder.disc_list import get_discs
+from mcdiscbuilder.archive import create_zip
 
 TEST_URL = "https://raw.githubusercontent.com/keotl/mcdisc/master/sample-disc-config.json"
 TARGET_DIRECTORY = "/tmp/testmcdisc"
@@ -9,9 +10,9 @@ discs = get_discs(TEST_URL)
 
 prepare_lang_file(discs, TARGET_DIRECTORY + "/assets/mcdisc/lang/en_us.lang")
 prepare_sounds_json(discs, TARGET_DIRECTORY + "/assets/mcdisc/sounds.json")
-prepare_pack_info(TARGET_DIRECTORY)
+prepare_pack_info(TARGET_DIRECTORY + "/pack.mcmeta")
 
 for disc in discs:
     downloader.download(disc['url'], TARGET_DIRECTORY + "/assets/mcdisc/sounds/" + disc['soundId'])
 
-
+create_zip(TARGET_DIRECTORY)
--- mcdiscbuilder/util.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-
-def get_json(url: str) -> list:
-    return requests.get(url).json()
